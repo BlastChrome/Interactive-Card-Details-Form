e452032fa315ebfa3e7fe2a5946c78bc1f21337a
Nicholas Jackson <nick@Nicholass-MacBook-Pro.local>
2023-03-01 13:54:45 -0800

    Added basic input and error handling

--- src/app/app.js
+++ src/app/app.js
@@ -1,48 +1,79 @@
 window.addEventListener('load', function() {
     // Card Graphic Elements
-    const cardNumberGraphic = document.getElementsByClassName('card-number')[0]; 
-    const cardNameGraphic  = document.getElementsByClassName('card-name')[0];
-    const cardDateGraphic  = document.getElementsByClassName('card-date')[0];
-    const cardCsvGraphic  = document.getElementById('card-back-numbers');  
-
+    const cardNumberGraphic = document.querySelector('.card-number');
+    const cardNameGraphic  = document.querySelector('.card-name');
+    const cardDateGraphic  = document.querySelector('.card-date');
+    const cardCsvGraphic  = document.querySelector('#card-back-numbers');  
+  
     // Error/Success Elements 
-    const errorNameElement = document.getElementsByClassName('error-name')[0]; 
-    console.log(errorNameElement);
-  
+    const errorNameElement = document.querySelector('.error-name');
+    const errorNumberElement = document.querySelector('.error-number');
+    const errorDateElement = document.querySelector('.error-date');
+    const errorCsvElement = document.querySelector('.error-csv');
+    
     // Card Input Fields 
-    const cardNameInput = document.getElementById('cardholdername');
-    const cardNumberInput = document.getElementById('cardnumber');
-    const cardMonthInput = document.getElementById('cardmonth');
-    const cardYearInput = document.getElementById('cardyear'); 
-    const cardCsvInput = document.getElementById('cardcsv');  
-  
+    const cardNameInput = document.querySelector('#cardholdername');
+    const cardNumberInput = document.querySelector('#cardnumber');
+    const cardMonthInput = document.querySelector('#cardmonth');
+    const cardYearInput = document.querySelector('#cardyear'); 
+    const cardCsvInput = document.querySelector('#cardcsv');  
+    
     // Submit Button 
-    const cardDataProcessButton = document.getElementById('process');
-    console.log(cardDataProcessButton);
+    const cardDataProcessButton = document.querySelector('#process');
   
-    // Arrays of Inputs and Graphic Elements
-    const cardGraphicElementsArray = [cardNumberGraphic , cardNameGraphic , cardDateGraphic , cardCsvGraphic , cardCsvGraphic ]; 
-    const cardInputElementsArray = [cardNameInput,cardNumberInput,cardMonthInput, cardYearInput, cardCsvInput];     
-
-
+    // Sends data from input field to be processed on click
     cardDataProcessButton.addEventListener('click', function(event){
-        event.preventDefault();  
-        ProcessInputData();
+      event.preventDefault();   
+      ProcessInputData();
     })
+  
+    // Processes Each Input, and displays if not entered correctly Errors
+    let ProcessInputData = () => { 
+      ProcessInputName(cardNameInput,cardNameGraphic,errorNameElement); 
+      ProcessInputNumber(cardNumberInput,cardNumberGraphic,errorNumberElement);  
+      ProcessInputDate(cardMonthInput,cardYearInput,cardDateGraphic,errorDateElement);
+      ProcessInputCsv(cardCsvInput,errorCsvElement);
+    }
+  
+    // Processes the Name input Field
+    let ProcessInputName = (nameInput,nameGraphic,error) => {
+      const inputText = nameInput.value; 
+      const nameRegex = /^[a-zA-Z\s]{1,20}$/;
+      const isValid = nameRegex.test(inputText);
+      UpdateGraphicAndError(isValid, inputText, nameGraphic, error);
+    }
+    
+    // Processes the Card Number input Field
+    let ProcessInputNumber = (numberInput,numberGraphic,error) => {
+      const inputText = numberInput.value; 
+      const numberRegex = /^(\d{4}\s){3}\d{4}$/;
+      const isValid = numberRegex.test(inputText); 
+      UpdateGraphicAndError(isValid, inputText, numberGraphic, error);
+    } 
 
-    let ProcessInputData = () => {
-        ProcessInputName(cardNameInput,cardNameGraphic,errorNameElement);
+    // Processes the Card Month input Field
+    let ProcessInputDate = (monthInput,yearInput,dateGraphic,error) => { 
+        const dateText =  monthInput.value + "/" + yearInput.value; 
+        const inputText = dateText;
+        const dateRegex = /^(0[1-9]|1[0-2])\/[0-9]{2}$/;
+        const isValid = dateRegex.test(inputText);
+        UpdateGraphicAndError(isValid, inputText, dateGraphic, error);
+      } 
+
+    let ProcessInputCsv = (csvInput,error) => {
+      const csvText = csvInput.value; 
+      const csvRegex = /^\d{3}$/; 
+      const isValid = csvRegex.test(csvText); 
+      UpdateGraphicAndError(isValid, csvText, cardCsvGraphic,error)
     }
 
-    let ProcessInputName = (nameInput,nameGraphic,error) => {
-        const inputText = nameInput.value; 
-        const nameRegex = /^[a-zA-Z\s]{1,30}$/;
-        const isValid = nameRegex.test(inputText);
-        if(!isValid) {
-            error.classList.toggle("show", true);
-        } else{
-            nameGraphic.innerText = inputText;
-            error.classList.toggle("show", false);
-        }
+    // Returns Error message above erroneous input field
+    let UpdateGraphicAndError = (isValid, inputText, graphicElement, errorElement) => {
+      if(!isValid) {
+        errorElement.classList.toggle("show", true);
+      } else{
+        graphicElement.innerText = inputText;
+        errorElement.classList.toggle("show", false);
+      }
     }
   });
